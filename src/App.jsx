6dd9326d0eedea41ea2a10a30d6a003ebb44a21b
Njatva143 <njatva143@gmail.com>
2025-04-29 23:49:11 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Layout from "./components/Layout";
 
 const App = () => {
+  const isLoggedIn = localStorage.getItem("token");
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route element={<Layout />}>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/*"
+        element={isLoggedIn ? <Layout /> : <Navigate to="/login" />}
+      >
+        <Route path="dashboard" element={<Dashboard />} />
       </Route>
     </Routes>
   );
