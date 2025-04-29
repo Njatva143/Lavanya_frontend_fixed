89f4876f7b38fe70db3b2604a40e636e0ca8c951
Njatva143 <njatva143@gmail.com>
2025-04-29 22:58:23 +0530

    Create Layout.jsx

---
+++ src/components/Layout.jsx
@@ -0,0 +1,23 @@
+// src/components/Layout.jsx
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
+const Layout = () => {
+  const isLoggedIn = localStorage.getItem("token");
+
+  if (!isLoggedIn) {
+    return <div className="p-4">Please login to access the dashboard.</div>;
+  }
+
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 p-4">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
